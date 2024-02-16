30cf9d7ab63310bebe902cb678c7814a4e6ce187
ca1c <gegs921@gmail.com>
2024-02-15 21:12:36 -0800

    change require statement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 const { Client, Collection, Events, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 const express = require('express');
